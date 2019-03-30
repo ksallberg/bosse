a463ff0eeb4b6d2072b7c0176cc5e943c454747b
Kristian Sallberg <kristian@purestyle.se>
2019-03-30 19:25:13 +0100

    add struct for tile

--- src/bosse.c
+++ src/bosse.c
@@ -2,23 +2,38 @@
 #define GLFW_INCLUDE_GLU
 #define TRAIL 15
 
+#define XAM 25
+#define YAM 25
+#define SIDE 10
+
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <math.h>
 #include <unistd.h>
 
-void draw_spot(float x0, float yy0, int ) {
+typedef struct Tile {
+  float x;
+  float y;
+  /* float a; */
+} Tile;
 
-  float side = 50.0f;
-  float x = side + x0;
-  float y = side + yy0;
+static Tile tiles[YAM][XAM];
+
+void draw_spot(float x0, float yy0, int color) {
+
+  float x = SIDE + x0;
+  float y = SIDE + yy0;
 
   glBegin(GL_POLYGON);
-  glColor4f(1, 1, 0, 100);
-  glVertex3f(x+side, y+side, 0);
-  glVertex3f(x-side, y+side, 0);
-  glVertex3f(x-side, y-side, 0);
-  glVertex3f(x+side, y-side, 0);
+  if(color==0) {
+    glColor4f(0, 1, 0, .5f);
+  } else {
+    glColor4f(0, 0, 1, .5f);
+  }
+  glVertex3f(x+SIDE, y+SIDE, 0);
+  glVertex3f(x-SIDE, y+SIDE, 0);
+  glVertex3f(x-SIDE, y-SIDE, 0);
+  glVertex3f(x+SIDE, y-SIDE, 0);
   glEnd();
   glFlush();
 }
@@ -74,6 +89,14 @@ int main(int argc, char** argv) {
   glLoadIdentity();
   glOrtho(0.0f, windowWidth, windowHeight, 0.0f, 0.0f, 1.0f);
 
+  /* Initialize tiles */
+  for(int i = 0; i < YAM; i ++) {
+    for(int j = 0; j < XAM; j ++) {
+      Tile t = {j * SIDE * 2, i * SIDE * 2};
+      tiles[i][j] = t;
+    }
+  }
+
   while (!glfwWindowShouldClose(window)) {
     /* Render here */
     int width, height;
@@ -98,8 +121,12 @@ int main(int argc, char** argv) {
     glfwGetFramebufferSize(window, &width, &height);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    draw_spot(1, 1);
-    draw_spot(102, 1);
+    for(int i = 0; i < YAM; i ++) {
+      for(int j = 0; j < XAM; j ++) {
+        Tile t = tiles[i][j];
+        draw_spot(t.x, t.y, (i + j) % 2);
+      }
+    }
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
